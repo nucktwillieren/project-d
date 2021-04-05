c0bfc6d62106579a7499910ca8f49f4c86a8b13c
Nathan <nucktwillieren@gmail.com>
2021-04-05 22:07:48 +0800

    pick a card

--- qcard_frontend/src/components/CardPick/CardPick.js
+++ qcard_frontend/src/components/CardPick/CardPick.js
@@ -17,7 +17,20 @@ const ColoredLine = ({ color }) => (
 const CardPick = () => {
   const initialToken = useSelector(state => state.token)
   const initialUser = useSelector(state => state.user)
-  const [card, setCard] = useState({})
+  const [username, setUsername] = useState()
+  const [email, setEmail] = useState()
+  const [name, setName] = useState()
+  const [gender, setGender] = useState()
+  const [birthday, setBirthday] = useState()
+  const [relationship, setRelationship] = useState()
+  const [Interest, setInterest] = useState()
+  const [club, setClub] = useState()
+  const [favoriteCourse, setFavoriteCourse] = useState()
+  const [favoriteCountry, setFavoriteCountry] = useState()
+  const [trouble, setTrouble] = useState()
+  const [exchange, setExchange] = useState()
+  const [trying, setTrying] = useState()
+
   const config = {
     headers: {
       'Content-Type': 'application/json',
@@ -36,9 +49,19 @@ const CardPick = () => {
         `http://localhost:8080/api/v1/pair/${initialUser}`,
         config
       )
-      console.log(resp.data)
-      setCard({ ...card, ...resp.data })
-      console.log(card)
+      setUsername(resp.data.username)
+      setEmail(resp.data.email)
+      setName(resp.data.name)
+      setGender(resp.data.gender)
+      setBirthday(resp.data.birthday)
+      setRelationship(resp.data.relationship)
+      setInterest(resp.data.interest)
+      setClub(resp.data.interest)
+      setFavoriteCourse(resp.data.favoriteCourse)
+      setFavoriteCountry(resp.data.favoriteCountry)
+      setTrouble(resp.data.trouble)
+      setExchange(resp.data.exchange)
+      setTrying(resp.data.trying)
     }
     const initialCreate = async () => {
       try {
@@ -56,18 +79,25 @@ const CardPick = () => {
   return (
     <div className="container" >
       <div className="profile-container">
-        <div><h2>{card.name}</h2></div>
+        <div><h2>{username}</h2></div>
         <ColoredLine color="rgba(0, 0, 0, 0.4)"></ColoredLine>
         <div className="avatar"><img className="embedded-logo" src={process.env.PUBLIC_URL + '/logo192.png'} alt="" /></div>
         <div className="form-box">
-          <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button className="btn btn-info btn-block login" type="submit">Sign In</button>
-          </form>
+          <div>Username: {username} </div>
+          <div>Email: {email} </div>
+          <div>Name: {name}</div>
+          <div>Gender: {gender}</div>
+          <div>Birthday: {birthday}</div>
+          <div>Relationship: {relationship}</div>
+          <div>Interest:{Interest} </div>
+          <div>Club: {club}</div>
+          <div>Favorite Courses: {favoriteCourse}</div>
+          <div>Favorite Country: {favoriteCountry}</div>
+          <div>Trouble: {trouble}</div>
+          <div>Exchange: {exchange}</div>
+          <div>Trying: {trying}</div>
           <ColoredLine color="rgba(0, 0, 0, 0.4)"></ColoredLine>
-          <div>No account yet?</div>
-          <div><a href="/register">Sign Up Now!</a></div>
+          <button className="btn btn-info btn-block login" type="submit">Send a Friend Request</button>
         </div>
       </div>
     </div >
